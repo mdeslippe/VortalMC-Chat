636ca8a0d27dc5e2a651fa9cda1e92a48cae1bef
razman121 <grimblycodes@gmail.com>
2020-12-10 00:01:42 -0500

    Created the plugin framework.

---
+++ src/main/java/com/vortalmc/chat/VortalMCChat.java
@@ -0,0 +1,25 @@
+package com.vortalmc.chat;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * The VortalMC-Chat plugin's main class.
+ * 
+ * @author Myles Deslippe
+ */
+public class VortalMCChat extends Plugin {
+	
+	/**
+	 * Called when the plugin is enabled.
+	 */
+	@Override
+	public void onEnable() {
+	}
+	
+	/**
+	 * Called when the plugin is disabled.
+	 */
+	public void onDisable() {
+	}
+	
+}
